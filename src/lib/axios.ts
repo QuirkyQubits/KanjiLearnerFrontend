d0c0a4ef443bbef0dd097855e514ef2518523491
Jona Huang <jonnyboy.345345@gmail.com>
2025-09-27 15:19:13 -0700

    modify API base url to split between dev/prod

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosHeaders, type InternalAxiosRequestConfig } from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000/kanjilearner/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
 
