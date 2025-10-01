8c2f6f9fe837557ea25b7860750addce0af1585f
Jona Huang <jonnyboy.345345@gmail.com>
2025-09-30 19:16:32 -0700

    fix failing frontend deployment

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -7,7 +7,6 @@ export default function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
-  const navigate = useNavigate();
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
